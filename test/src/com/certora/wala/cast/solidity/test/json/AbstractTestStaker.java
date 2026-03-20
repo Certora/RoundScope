e0039f7e0ce3ed89b82498c662617b8144148851
Julian <julian@certora.com>
2026-03-20 14:10:01 -0400

    comments

--- test/src/com/certora/wala/cast/solidity/test/json/AbstractTestStaker.java
+++ test/src/com/certora/wala/cast/solidity/test/json/AbstractTestStaker.java
@@ -35,15 +35,19 @@ public abstract class AbstractTestStaker {
 		
 		DocumentContext jsonParser = JsonPath.using(configuration).parse(output);
         
+		// at least some mulDivUp should round up. 
 		JSONArray result = jsonParser.read("$.graphs[*].nodes[*].metadata[?(@.method == '<Code body of function mulDivUp>' && @.return == 'Up') ]");		
 		assert !result.isEmpty();
 		
+		// no mulDivUp should round down. 
 		result = jsonParser.read("$.graphs[*].nodes[*].metadata[?(@.method == '<Code body of function mulDivUp>' && @.return == 'Down') ]");		
 		assert result.isEmpty();
 
+		// at least some mulDivDown should round down. 
 		result = jsonParser.read("$.graphs[*].nodes[*].metadata[?(@.method == '<Code body of function mulDivDown>' && @.return == 'Down') ]");		
 		assert !result.isEmpty();
 		
+		// no mulDivDown should round up. 
 		result = jsonParser.read("$.graphs[*].nodes[*].metadata[?(@.method == '<Code body of function mulDivDown>' && @.return == 'Up') ]");		
 		assert result.isEmpty();
 
