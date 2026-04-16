6383722e89da2c3abf30e3363f42a4ed64f8d95e
Julian <julian@certora.com>
2026-04-16 12:21:13 -0400

    make test check the generated json

--- test/src/com/certora/wala/cast/solidity/test/json/TestForcedRounding.java
+++ test/src/com/certora/wala/cast/solidity/test/json/TestForcedRounding.java
@@ -1,6 +1,9 @@
 package com.certora.wala.cast.solidity.test.json;
 
+import org.json.JSONArray;
+
 import com.certora.wala.cast.solidity.test.AbstractTestForcedRounding;
+import com.jayway.jsonpath.DocumentContext;
 
 public class TestForcedRounding extends AbstractJsonTest implements AbstractTestForcedRounding {
 
@@ -9,4 +12,24 @@ public class TestForcedRounding extends AbstractJsonTest implements AbstractTest
 		return "test/data/ForcedRounding";
 	}
 
+	@Override
+	public void checkResult(DocumentContext jsonParser) {
+		JSONArray result = jsonParser.read("$.graphs[*].nodes[*].metadata[?(@.method == '<Code body of function forcedDown>' && @.return == 'Down') ]");		
+		System.err.println(result);
+		assert !result.isEmpty();
+
+		result = jsonParser.read("$.graphs[*].nodes[*].metadata[?(@.method == '<Code body of function forcedDown>' && @.return != 'Down') ]");		
+		System.err.println(result);
+		assert result.isEmpty();
+
+		result = jsonParser.read("$.graphs[*].nodes[*].metadata[?(@.method == '<Code body of function forcedUp>' && @.return == 'Up') ]");		
+		System.err.println(result);
+		assert !result.isEmpty();
+
+		result = jsonParser.read("$.graphs[*].nodes[*].metadata[?(@.method == '<Code body of function forcedUp>' && @.return != 'Up') ]");		
+		System.err.println(result);
+		assert result.isEmpty();
+	}
+
+	
 }
