b98404260fddf4d0062bc6d2a34fc5705b55c420
Julian Dolby (Certora work) <julian@certora.com>
2026-05-21 17:45:23 -0400

    license (#60)

--- src/com/certora/wala/cast/solidity/tree/CallableEntity.java
+++ src/com/certora/wala/cast/solidity/tree/CallableEntity.java
@@ -1,3 +1,15 @@
+/*
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://eclipse.org.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License, v. 2.0 are satisfied: {name license(s), version(s), and
+ * exceptions or additional permissions here}.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package com.certora.wala.cast.solidity.tree;
 
 import java.util.ArrayList;
